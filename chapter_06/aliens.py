486f3bae5d5e8550b3c9515d9fdfd5f1fe74b8cf
Marek Kwiatek <marek.kwiatek@yahoo.com>
2026-06-19 17:38:18 +0200

    Changes in the aliens.py

--- chapter_06/aliens.py
+++ chapter_06/aliens.py
@@ -11,8 +11,8 @@ aliens = []
 print("Clearing the list of aliens")
 
 for alien in range(30):
-    new_alien = {"color": "green", "points": 5, "speed": "slow"}
-    aliens.append(new_alien)
+    alien = {"color": "green", "points": 5, "speed": "slow"}
+    aliens.append(alien)
 
 for alien in aliens[:5]:
     print(alien)
